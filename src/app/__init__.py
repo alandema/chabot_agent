8d650381d8de64a7dcaf40e8354d83cbf4612521
alandema <alann.maman@gmail.com>
2024-11-03 16:19:30 -0300

    making it work

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,12 +1,17 @@
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
-import os 
-  
-file_path = os.path.abspath(os.getcwd())+"src/todo.db"
-  
-app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path 
-db = SQLAlchemy(app) 
-  
-  
-from app import routes 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+file_path = os.path.abspath(os.getcwd())+"\\src\\todo.db"
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+db = SQLAlchemy(app)
+
+
+app.app_context().push()
+
+db.create_all()
+
+from app import routes  # noqa
