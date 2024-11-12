29ae70682f6f278149b23cf580d3f444d654d6fe
Alan <alann.maman@gmail.com>
2024-11-12 19:19:18 -0300

    last modifications

--- src/run.py
+++ src/run.py
@@ -5,56 +5,61 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from dotenv import load_dotenv
+load_dotenv('config.env')
 from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, trim_messages, AnyMessage, ToolMessage
 from langgraph.graph.message import add_messages
 from langgraph.graph import MessagesState
 from langgraph.checkpoint.memory import MemorySaver
-load_dotenv('config.env')
 
-memory = MemorySaver()
-# class MessagesState(TypedDict):
-#     messages: Annotated[list[AnyMessage], add_messages]
+class TypedDictState(TypedDict):
+    messages: list
+    name: str
+    mood: Literal["happy","sad"]
 
-class MessagesState(MessagesState):
-    pass
+def node_1(state):
+    print("---Node 1---")
+    return {"name": state['name'] + " is ... "}
 
-def tool_calling_llm(state: MessagesState):
-    return {"messages": [chain.invoke(state["messages"])]}
+def node_2(state):
+    print("---Node 2---")
+    return {"mood": "happy"}
 
-def multiply(a: int, b: int) -> int:
-    """Multiply a and b.
+def node_3(state):
+    print("---Node 3---")
+    return {"mood": "sad"}
 
-    Args:
-        a: first int
-        b: second int
-    """
-    return a * b
+class UserName(TypedDict):
+    name: str
 
-def add(a: int, b: int) -> int:
-    """Sum a and b.
 
-    Args:
-        a: first int
-        b: second int
-    """
-    return a + b
 
-builder = StateGraph(MessagesState)
-builder.add_node("tool_calling_llm", tool_calling_llm)
-builder.add_node("tools", ToolNode([multiply, add]))
 
-builder.add_edge(START, "tool_calling_llm")
-builder.add_conditional_edges(
-    "tool_calling_llm",
-    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
-    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
-    tools_condition,
-)
-builder.add_edge("tools", "tool_calling_llm")
-builder.add_edge("tool_calling_llm", END)
+def decide_mood(state) -> Literal["node_2", "node_3"]:
+        
+    # Here, let's just do a 50 / 50 split between nodes 2, 3
+    if random.random() < 0.5:
 
-graph = builder.compile(checkpointer=memory)
+        # 50% of the time, we return Node 2
+        return "node_2"
+    
+    # 50% of the time, we return Node 3
+    return "node_3"
+
+# Build graph
+builder = StateGraph(TypedDictState)
+builder.add_node("node_1", node_1)
+builder.add_node("node_2", node_2)
+builder.add_node("node_3", node_3)
+
+# Logic
+builder.add_edge(START, "node_1")
+builder.add_conditional_edges("node_1", decide_mood)
+builder.add_edge("node_2", END)
+builder.add_edge("node_3", END)
+
+# Add
+graph = builder.compile()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash",
@@ -65,8 +70,6 @@ llm = ChatGoogleGenerativeAI(
 )
 
 
-llm_with_tools = llm.bind_tools([multiply, add])
-
 prompt = ChatPromptTemplate(
     [
         (
@@ -76,8 +79,8 @@ prompt = ChatPromptTemplate(
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
-
-chain = prompt | llm_with_tools
+llm_oputput = llm.with_structured_output(UserName)
+chain = prompt | llm
 
 
 def main(message, id="abc123"):
@@ -90,5 +93,5 @@ def main(message, id="abc123"):
 if __name__ == '__main__':
 
     # main.app.run(debug=True)
-    for messages in ['Hello, my name is Alan.','Sum 3 plus 5', 'now multiply by 2']:
+    for messages in ['Hello, my name is Alan.']:
         main(messages, id="abc123")
