95ff8795e99316d719340bf2e2da44d8b0b7f494
alandema <alann.maman@gmail.com>
2024-11-12 21:31:58 -0300

    change

--- src/run.py
+++ src/run.py
@@ -1,50 +1,55 @@
 # from app.interface import main
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import MessagesState
+from langgraph.graph.message import add_messages
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, trim_messages, AnyMessage, ToolMessage
+from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
 from typing import TypedDict, Literal, Annotated
 import random
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from dotenv import load_dotenv
 load_dotenv('config.env')
-from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, trim_messages, AnyMessage, ToolMessage
-from langgraph.graph.message import add_messages
-from langgraph.graph import MessagesState
-from langgraph.checkpoint.memory import MemorySaver
+
 
 class TypedDictState(TypedDict):
     messages: list
     name: str
-    mood: Literal["happy","sad"]
+    mood: Literal["happy", "sad"]
+
 
 def node_1(state):
     print("---Node 1---")
     return {"name": state['name'] + " is ... "}
 
+
 def node_2(state):
     print("---Node 2---")
     return {"mood": "happy"}
+
 
 def node_3(state):
     print("---Node 3---")
     return {"mood": "sad"}
 
+
 class UserName(TypedDict):
     name: str
 
 
-
-
 def decide_mood(state) -> Literal["node_2", "node_3"]:
-        
+
     # Here, let's just do a 50 / 50 split between nodes 2, 3
     if random.random() < 0.5:
 
         # 50% of the time, we return Node 2
         return "node_2"
-    
+
     # 50% of the time, we return Node 3
     return "node_3"
+
 
 # Build graph
 builder = StateGraph(TypedDictState)
@@ -89,6 +94,7 @@ def main(message, id="abc123"):
     for message in output["messages"]:
         if isinstance(message, AIMessage):
             print(message.content)
+
 
 if __name__ == '__main__':
 
