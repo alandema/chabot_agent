c7da21f8ae8cfec69a147ef448bfc38619accefd
Alan <alann.maman@gmail.com>
2024-11-14 23:14:24 -0300

    change in html

--- src/app/static/js/passwords.js
+++ src/app/static/js/passwords.js
@@ -11,16 +11,18 @@ function validateForm() {
     return true;
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    const path = window.location.pathname;
 
-document.addEventListener('DOMContentLoaded', function ()
-{
-    document.getElementById('confirm_password').addEventListener('focus', function() {
-        const errorElement = document.getElementById('password_match_error');
-        errorElement.style.display = 'none';
-    });
-    
-    document.getElementById('password').addEventListener('focus', function() {
-        const errorElement = document.getElementById('password_match_error');
-        errorElement.style.display = 'none';
-    });
+    if (path.includes('/login')) {
+        document.getElementById('login-form').addEventListener('focusin', function() {
+            const errorElement = document.getElementById('wrong_password_error');
+            errorElement.style.display = 'none';
+        });
+    } else if (path.includes('/register')) {
+        document.getElementById('register-form').addEventListener('focusin', function() {
+            const errorElement = document.getElementById('password_match_error');
+            errorElement.style.display = 'none';
+        });
+    }
 }, false);
